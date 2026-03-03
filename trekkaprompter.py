3f9194e29fe42f09a54ebc6c4187530e686476a1
Shawn <vahnatai@users.noreply.github.com>
2026-03-03 13:21:06 -0330

    allow run with no arguments (first 4 eps)

--- trekkaprompter.py
+++ trekkaprompter.py
@@ -11,12 +11,13 @@ DEFAULT_COUNT = 4
 NEXT_RELEASE_START_TOKEN = 'released in all'
 NEXT_RELEASE_TITLE_START_TOKEN = 'title="'
 NEXT_RELEASE_TITLE_END_TOKEN = '"'
+FIRST_EP_NAME = 'The Man Trap (episode)'
 
 argp = argparse.ArgumentParser(
     prog='trekkaprompter',
     description='calculates watch orders for Star Trek by scraping the Memory Alpha fandom wiki'
 )
-argp.add_argument('page_title')
+argp.add_argument('page_title', type=str, nargs="?")
 argp.add_argument('count', type=int, nargs="?", default=DEFAULT_COUNT)
 
 def get_substring_before(source, prefix):
@@ -54,8 +55,18 @@ def get_next_released(page) :
 if __name__ == '__main__' :
     args = argp.parse_args()
 
-    page_title = args.page_title
+    count = args.count
+
     page = None
+
+    if args.page_title :
+        page_title = args.page_title
+    else :
+        page_title = FIRST_EP_NAME
+        page = fandom.page(page_title)
+        describe(page)
+        count -= 1
+
     if not page_title.endswith(EP_TITLE_MARKER) :
         try :
             page = fandom.page(page_title + EP_TITLE_MARKER)
@@ -63,11 +74,6 @@ if __name__ == '__main__' :
             pass
     if not page :
         page = fandom.page(page_title)
-
-    if args.count :
-        count = args.count
-    else :
-        count = DEFAULT_COUNT
 
     for i in range(count) :
         try :
