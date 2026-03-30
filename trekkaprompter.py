8ff9d2ea46bcd6ecc6a387b157b498dcb0c71de1
Shawn <vahnatai@users.noreply.github.com>
2026-03-30 12:19:13 -0230

    minor: no space before colon

--- trekkaprompter.py
+++ trekkaprompter.py
@@ -22,20 +22,20 @@ argp = argparse.ArgumentParser(
 argp.add_argument('page_title', type=str, nargs="?")
 argp.add_argument('count', type=int, nargs="?", default=DEFAULT_COUNT)
 
-def describe(page) :
+def describe(page):
     infobox = page.content['infobox']
     first_line, _, remainder = infobox.partition('\n')
     episode_match = EPISODE_RE.match(first_line)
-    if episode_match :  # episode
+    if episode_match:  # episode
         series, episode_num = episode_match.groups()
         print(f'{series} {episode_num} "{page.title.replace(EP_TITLE_MARKER, "")}"')
-    else :                  # movie?
+    else:              # movie?
         movie_num_line = remainder.split('\n')[1]
         movie_num = MOVIE_NUM_RE.match(movie_num_line).group(1)
         print(f'FLM {movie_num} "{page.title}"')
     sys.stdout.flush()
 
-def get_next_released(page) :
+def get_next_released(page):
     match = NEXT_RELEASED_RE.search(page.html)
     if match:
         next_title = match.group(1)
@@ -57,23 +57,23 @@ if __name__ == '__main__' :
         describe(page)
         count -= 1
 
-    if not page_title.endswith(EP_TITLE_MARKER) :
-        try :
+    if not page_title.endswith(EP_TITLE_MARKER):
+        try:
             page = fandom.page(page_title + EP_TITLE_MARKER)
-        except Exception :
+        except Exception:
             pass
     if page is None:
-        try :
+        try:
             page = fandom.page(page_title)
-        except fandom.error.PageError as e :
+        except fandom.error.PageError as e:
             print(e)
             sys.exit(1)
 
-    for _ in range(count) :
-        try :
+    for _ in range(count):
+        try:
             page = get_next_released(page)
             describe(page)
-        except Exception as e :
+        except Exception as e:
             print('Exception:', e)
             traceback.print_exception(e)
             sys.exit(1)
